def6af29f323de7406b303d0b020b629151c8ca8
Christopher Wood <cjwood01@gmail.com>
2020-02-29 19:52:26 -0600

    Made seperate route files. got save function to work

---
+++ Develop/routes/APIroutes.js
@@ -0,0 +1,31 @@
+const path = require("path")
+const fs = require("fs")
+module.exports = function(app){
+  app.get("/api/notes", function(req, res) {
+    console.log(res)
+    // notes = res
+  });
+    // Create New Notes - takes in JSON input
+  app.post("/api/notes", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var notes = []
+        const newNote = req.body;
+        notes.push(newNote);
+        console.log("outsideWriteFile")
+        fs.writeFile("./db/db.json", JSON.stringify(notes), function(err){
+            if (err){
+                console.log(err)
+            }
+            else {
+                console.log("OK")
+            }
+        })
+        console.log(newNote);
+        res.json(notes);
+      });
+      //DELETE
+    app.delete("/api/notes/:id", function(req, res){
+        var noteId = req.params.id
+    })
+}
--- Develop/server.js
+++ Develop/server.js
@@ -2,53 +2,24 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3030;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json()); //MIDDLEWARE
 app.use(express.static("public"));
-const notes = []
-// Routes
-// =============================================================
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-  // Endpoints to displays all notes
-  app.get("/api/notes", function(req, res) {
-    
-    console.log(res)
-    
-  });
-  app.post("/api/notes", function(req, res){
-    console.log(req.body)
-  })
-  // Endpoint to display a single note, or returns false
-  app.get("/api/notes/:id", function(req, res) {
-    var chosen = req.params.notes;
-    console.log(chosen);
-    return res.json(false);
-  });
-  // Create New Notes - takes in JSON input
-  app.post("/api/notes", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    const newNote = req.body;
-    notes.push(newNote);
-    fs.writeFile("../../../db/db.json", JSON.stringify(notes)).then(function(res){
-      console.log(res)
-    })
-    console.log(newNote);
-    res.json(notes);
-  });
+
+require("./routes/HTMLroutes")(app)
+require("./routes/APIroutes")(app)
+
+
   // Starts the server to begin listening
   // =============================================================
   app.listen(PORT, function() {
     console.log(`App listening on ${PORT}`);
   });
+
